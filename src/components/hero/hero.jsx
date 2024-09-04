8958009ba71aa1d7a1264aad6e3aae6e1a1f559c
Ryisan Champion <44957246+ryisan2@users.noreply.github.com>
2024-09-04 03:21:39 -0400

    Update hero.jsx
    naked

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -46,7 +46,7 @@ const Hero = () => {
         <a onClick={toggleModal} className={styles.contactBtn}>Contact Me</a>
       </div>
       <img
-        src={getImageUrl("./assets/heroImage.png")}
+        src={getImageUrl("heroImage.png")}
         alt="Hero Image"
         className={styles.heropic}
       />
